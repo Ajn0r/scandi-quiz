957a5effcc405d7c5e945404cc52e3d5129263be
Ajn0r <ronjasjogren@hotmail.com>
2022-02-27 20:03:29 +0100

    add javaScript for taking the player to the contact page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// ------- JavaScript for games first page ------------
+
 const quiz = document.getElementById('game-area');
 const startPage = document.getElementById('start');
 const startButton = document.getElementById('play-btn');
@@ -5,11 +7,13 @@ const header = document.getElementById('header-text');
 const mainGameSection = document.getElementById('main-game');
 const howToPage = document.getElementById('about');
 const howToButton = document.getElementById('how-to-btn');
-
+const contactBtn = document.getElementById('contact-btn');
 
 startButton.addEventListener('click', startQuiz);
 header.addEventListener('click', startOver);
 howToButton.addEventListener('click', howToPlay);
+contactBtn.addEventListener('click', goToContact); 
+
 /**
  * Function for starting the quiz, adding the hide class to first section and displaying quiz section.
  */
@@ -23,7 +27,6 @@ function startQuiz () {
 function startOver () {
     startPage.className.remove('hide');
 }
-
 /**
  * Function that takes the player to the How to play section, hiding all other sections.
  */
@@ -31,7 +34,11 @@ function howToPlay () {
     mainGameSection.classList.add('hide');
     howToPage.classList.remove('hide');
 }
+/**
+ * Function that takes the player to the contact page
+ */
+function goToContact() {
+    window.location.href="contact.html";
+    }
 
-
-
-// Trying out code under here
+    // -------- JavaScript for displaying quiz here ---------
