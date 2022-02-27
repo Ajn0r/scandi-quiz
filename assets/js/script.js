7942b7e4edccaddc5f95ddd938546cad69553c10
Ajn0r <ronjasjogren@hotmail.com>
2022-02-27 15:25:12 +0100

    remove some code that was only used for testing, and add comments to functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,20 @@
 const quiz = document.getElementById('game-area');
 const startPage = document.getElementById('start');
 const startButton = document.getElementById('play-btn');
-const howToBtn = document.getElementById('how-to-btn');
 const header = document.getElementById('header-text');
 
-
 startButton.addEventListener('click', startQuiz);
-howToBtn.addEventListener('click', showHowTo);
 header.addEventListener('click', startOver);
-
+/**
+ * Function for starting the quiz, adding the hide class to first section and displaying quiz section.
+ */
 function startQuiz () {
     startPage.classList.add('hide');
     quiz.classList.remove('hide');
 }
-
+/**
+ * Function that takes the player back to the first start 'page' when header is clicked
+ */
 function startOver () {
-    
     startPage.className.remove('hide');
-
 }
